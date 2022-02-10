88f5214c392ac77b1e1c820202cbae08ee7e6caa
Jackson Magnabosco <jackson.magnabosco@globalsys.com.br>
2022-02-10 20:57:10 -0300

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Automação para bater ponto automático
+===============================================
+- Automação criada com Python utilizando a biblioteca Selenium
+
+- Para rodar a automação em horarios específicos foi utilizado o agendador de tarefas do Windows
+
+- Para utilizar o agendador é só importar o xml no agendador em ações importar tarefa
+--------------------
---
+++ index.py
@@ -0,0 +1,35 @@
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+from selenium.common.exceptions import TimeoutException
+import os
+
+login = os.getenv('LOGIN_PONTO')
+password = os.getenv('PASSWORD_PONTO')
+
+browser = webdriver.Chrome()
+browser.get("https://pontosecullum4-01.secullum.com.br/ponto4web/821220936#login")
+delay = 3
+try:
+    login_box  = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login-form-campos"]/p[2]/input')))
+    login_box.send_keys(login)
+    login_box.submit()
+
+    password_box  = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login-form-campos"]/p[3]/input')))
+    password_box.send_keys(password)
+    password_box.submit()
+
+    login_button  = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="login"]')))
+    login_button.click()
+
+    menu_button = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="itens-menu"]/li[2]/a')))
+    menu_button.click()
+
+    register_button = WebDriverWait(browser, delay).until(EC.presence_of_element_located((By.XPATH, '//*[@id="registrar"]')))
+    register_button.click()
+
+except TimeoutException:
+    print ("Loading took too much time!")
+
+
